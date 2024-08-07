4facab513ba2b9ebf080fc1f2576042e0cdb622d
Ausslanderr <paciencia.2001@gmail.com>
2024-08-07 18:26:41 -0300

    removendo send_data.py

--- python-container/send_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import requests
-
-# Carregar dados de jogos
-data = pd.read_csv('steam_games_prices.csv')
-
-# Enviar dados para o servidor Node.js
-response = requests.post('http://localhost:3000/upload', json=data.to_dict(orient='records'))
-print(response.status_code)
